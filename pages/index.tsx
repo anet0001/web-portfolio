e2a75c4b78ed37f22555ff3d2dff2849090d0af8
Nonso Anetoh <anet0001@algonquinlive.com>
2023-05-13 19:32:07 -0400

    updates scss styling for multiple components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,41 @@
+import Clock from "@/components/Clock";
+import ProjectList from "@/components/ProjectList";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
+import TitleCard from "@/components/TitleCard";
+import Loader from "@/components/shared/Loader";
+import { gsap } from "gsap";
 import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
+
+type Timeline = gsap.core.Timeline | undefined;
 
 export default function Home() {
+  const [showLoader, setShowLoader] = useState(true);
+  const [timeline, setTimeline] = useState<Timeline | undefined>();
+  const titleCardRef = useRef(null);
+  const scope = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline();
+      setTimeline(tl);
+    }, scope);
+
+    return () => ctx.revert();
+  }, []);
+
+  useEffect(() => {
+    timeline && timeline.reversed(!showLoader);
+  }, [timeline, showLoader]);
+
+  useEffect(() => {
+    timeline &&
+      timeline.to(".test", {
+        y: 100,
+        duration: 3,
+      });
+  }, [timeline, showLoader]);
+
   return (
     <>
       <Head>
@@ -9,7 +44,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>My Next App</main>
+      <Loader showLoader={showLoader} setShowLoader={setShowLoader} />
+      <main ref={scope}>
+        <ThemeSwitcher />
+        <ProjectList />
+        <Clock />
+        <TitleCard ref={titleCardRef} />
+      </main>
     </>
   );
 }
