954d151b42194f2f6e9266d9b2645d9743ba6937
Nonso Anetoh <anet0001@algonquinlive.com>
2023-05-13 19:31:25 -0400

    added a themeswitcher

---
+++ components/ThemeSwitcher.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from "react";
+
+interface TSProps {}
+
+interface Theme {
+  name: string;
+}
+
+const ThemeSwitcher: FC<TSProps> = () => {
+  const themes: Theme[] = [{ name: "adsd" }, { name: "adsd" }];
+
+  return (
+    <div className="theme-switcher">
+      {themes.map(({ name }, index) => (
+        <button
+          className="theme-switcher__switch"
+          key={index}
+          onClick={() => console.log(name)}
+        >
+          {}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default ThemeSwitcher;
