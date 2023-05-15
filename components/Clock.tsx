1222eef57ab2d643433145d5a164c40ff38b2387
Nonso Anetoh <anet0001@algonquinlive.com>
2023-05-15 17:23:08 -0400

    created clock component

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -1,11 +1,45 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from "react";
 
 interface ClockProps {}
 
-const Clock:FC<ClockProps> = () => {
-  return (
-    <div>Clock</div>
-  )
-}
+const Clock: FC<ClockProps> = () => {
+  const [ottawaTime, setOttawaTime] = useState("");
+  const [workingHours, setWorkingHours] = useState(false);
 
-export default Clock
+  useEffect(() => {
+    const displayCurrentTime = () => {
+      const date = new Date();
+      const ottawaTime = date.toLocaleTimeString("en-US", {
+        timeZone: "America/Toronto",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: false,
+      });
+
+      const hour = date.getHours();
+      const workingHours = hour >= 7 && hour <= 23;
+
+      setWorkingHours(workingHours);
+      setOttawaTime(ottawaTime);
+    };
+
+    const timer = setInterval(displayCurrentTime, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className="clock">
+      <span className="time">{ottawaTime}</span>
+      <span
+        className={`status ${
+          workingHours ? "status--working" : "status--resting"
+        }`}
+      />
+    </div>
+  );
+};
+
+export default Clock;
