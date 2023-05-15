2dc581403811a4156fa88c1f19a3e79a1d1c589a
Nonso Anetoh <anet0001@algonquinlive.com>
2023-05-15 17:23:14 -0400

    created logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,43 @@
+import { Timeline } from "@/types/animation";
+import { gsap } from "gsap";
+import React, {
+  ForwardRefRenderFunction,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+
+interface TCProps {
+  showLoader: boolean;
+}
+
+const Logo: ForwardRefRenderFunction<HTMLDivElement, TCProps> = (
+  { showLoader },
+  ref
+) => {
+  const scope = useRef(null);
+
+  useEffect(() => {
+    if (showLoader) return;
+
+    const ctx = gsap.context(() => {
+      let timeline = gsap.timeline();
+
+      timeline.from(".text", {
+        autoAlpha: 0,
+        y: -100,
+      });
+    }, scope);
+
+    return () => ctx.revert();
+  }, [showLoader]);
+
+  return (
+    <div className="logo" ref={scope}>
+      <span className="text">Nonso Anetoh</span>
+    </div>
+  );
+};
+
+export default forwardRef(Logo);
