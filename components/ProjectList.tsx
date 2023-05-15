657815310c9c56979ce1ad447ac294abfefbcb99
Nonso Anetoh <anet0001@algonquinlive.com>
2023-05-15 17:23:22 -0400

    created project-list component

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -1,9 +1,37 @@
-import React, { FC } from "react";
+import Link from "next/link";
+import React, { FC, useState } from "react";
+import ProjectShowcase from "./ProjectShowcase";
 
 interface PLProps {}
 
 const ProjectList: FC<PLProps> = () => {
-  return <div>ProjectList</div>;
+  const projects = [
+    { name: "Nonso Anetoh", link: "/" },
+    { name: "Rexvirgo Media", link: "" },
+    { name: "Jasmine College", link: "" },
+    { name: "Shop Rebellis", link: "" },
+  ];
+
+  const [hoveredProject, setHoveredProject] = useState<number | null>(null);
+
+  return (
+    <div className="project-list">
+      <ul>
+        {projects.map(({ name, link }, index) => {
+          return (
+            <li
+              key={index}
+              onMouseEnter={() => setHoveredProject(index + 1)}
+              onMouseLeave={() => setHoveredProject(null)}
+            >
+              <Link href={link}>{name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      <ProjectShowcase hoveredProject={hoveredProject} />
+    </div>
+  );
 };
 
 export default ProjectList;
